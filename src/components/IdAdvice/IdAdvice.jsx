5d3233423560cab989b47564a7f1406f7b92751d
Ana Luís Guerreiro <analuisguerreiro@hotmail.com>
2023-11-22 18:40:03 +0000

    Styles

--- src/components/IdAdvice/IdAdvice.jsx
+++ src/components/IdAdvice/IdAdvice.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function IdAdvice(props) {
   return (
-    <div className="Advice">
+    <div className="IdAdvice">
       <h2>Advice #234</h2>
     </div>
   );
